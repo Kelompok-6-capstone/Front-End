aeb846512a9081a226aec0dbfbaec8ec6954c712
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-12-01 16:27:17 +0700

    Fix import VerifyOtpPage in AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from '../pages/auth/LoginPage';
 import RegisterPage from '../pages/auth/RegisterPage';
 import ForgotPasswordPage from '../pages/auth/ForgotPasswordPage';
-import VerifyOTPPage from '../pages/auth/VerifyOTPPage';
+import VerifyOtpPage from "../pages/auth/VerifyOTPPage";
 import LandingPage from '../pages/user/LandingPage';
 import Dashboard from '../pages/admin/Dashboard';
 
@@ -13,7 +13,7 @@ const AppRoutes = () => (
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/verify-otp" element={<VerifyOTPPage />} />
+        <Route path="/verify-otp" element={<VerifyOtpPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
 );
