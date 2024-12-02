517683d8b6873d7802af57029381da7ac91ab5bd
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-12-02 17:22:44 +0700

    Add TagSection component to Beranda for displaying common issues

---
+++ src/components/user/beranda/TagSection.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+
+export default function TagSection() {
+    return (
+        <>
+            <div className="text-cyan-900 text-base font-semibold mt-8 text-center lg:text-left">Masalah Yang Sering Dihadapi</div>
+            <div className="flex flex-wrap gap-4 justify-center md:justify-start p-4">
+                <img
+                    src="/images/user/beranda/tags/stress.svg"
+                    alt="gambar tags"
+                    className="w-[80px] h-[80px] md:w-[116px] md:h-[116px] object-contain"
+                />
+                <img
+                    src="/images/user/beranda/tags/depresi.svg"
+                    alt="gambar tags"
+                    className="w-[80px] h-[80px] md:w-[116px] md:h-[116px] object-contain"
+                />
+                <img
+                    src="/images/user/beranda/tags/trauma.svg"
+                    alt="gambar tags"
+                    className="w-[80px] h-[80px] md:w-[116px] md:h-[116px] object-contain"
+                />
+                <img
+                    src="/images/user/beranda/tags/adiksi.svg"
+                    alt="gambar tags"
+                    className="w-[80px] h-[80px] md:w-[116px] md:h-[116px] object-contain"
+                />
+                <img
+                    src="/images/user/beranda/tags/gangguan-kecemasan.svg"
+                    alt="gambar tags"
+                    className="w-[80px] h-[80px] md:w-[116px] md:h-[116px] object-contain"
+                />
+                <img
+                    src="/images/user/beranda/tags/pengembangan-diri.svg"
+                    alt="gambar tags"
+                    className="w-[80px] h-[80px] md:w-[116px] md:h-[116px] object-contain"
+                />
+                <img
+                    src="/images/user/beranda/tags/gangguan-mood.svg"
+                    alt="gambar tags"
+                    className="w-[80px] h-[80px] md:w-[116px] md:h-[116px] object-contain"
+                />
+                <img
+                    src="/images/user/beranda/tags/pengasuhan.svg"
+                    alt="gambar tags"
+                    className="w-[80px] h-[80px] md:w-[116px] md:h-[116px] object-contain"
+                />
+                <img
+                    src="/images/user/beranda/tags/lihat-lainnya.svg"
+                    alt="gambar tags"
+                    className="w-[80px] h-[80px] md:w-[116px] md:h-[116px] object-contain"
+                />
+            </div>
+        </>
+    )
+}
