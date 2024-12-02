4ce37ae76b5ded463f1c4511ce88174310b9bd96
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-12-02 07:57:36 +0700

    Add Artikel component with Navbar and Sidebar

---
+++ src/pages/user/dashboard/Artikel.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../../../components/user/beranda/Navbar'
+import Sidebar from '../../../components/user/beranda/Sidebar'
+
+export default function Artikel() {
+    return (
+        <>
+            <Navbar />
+            <Sidebar />
+            <div className="w-full lg:mx-2 lg:ps-[270px]">
+                <h1>Artikel</h1>
+            </div>
+        </>
+    )
+}
