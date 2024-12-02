2c27ac0ceaef6605e91eb5c83470e816deaa13c2
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-12-02 07:57:14 +0700

    Add Beranda component for user dashboard

---
+++ src/pages/user/dashboard/Beranda.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Beranda() {
+    return (
+        <>
+            <div className="w-full lg:mx-2 lg:ps-[270px]">
+                <h1>Beranda</h1>
+            </div>
+        </>
+    )
+}
