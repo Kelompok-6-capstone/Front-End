09bdfa604134049d8ae08e4be8ce38facd132c90
Arjun Nafi <arjunnafi04@gmail.com>
2024-12-02 16:50:16 +0700

    refactor admin-list-user and modif table

--- src/components/admin/PatientTable.jsx
+++ src/components/admin/PatientTable.jsx
@@ -1,47 +1,9 @@
-import { useState } from "react";
-import { ArrowUp, ArrowDown } from 'lucide-react';
+import React from "react";
+import useSortData from "../../hooks/useSortData";
+import { getSortIcon } from "../../utils/getSortIcon";
 
 const PatientTable = ({ data }) => {
-  const [sortConfig, setSortConfig] = useState({
-    key: null,
-    direction: null,
-  });
-
-  const sortedData = [...data].sort((a, b) => {
-    if (!sortConfig.key) return 0;
-
-    const aValue = a[sortConfig.key];
-    const bValue = b[sortConfig.key];
-
-    if (sortConfig.key === "usia") {
-      return sortConfig.direction === "ascending"
-        ? Number(aValue) - Number(bValue)
-        : Number(bValue) - Number(aValue);
-    }
-
-    return sortConfig.direction === "ascending"
-      ? aValue.localeCompare(bValue)
-      : bValue.localeCompare(aValue);
-  });
-
-  const requestSort = (key) => {
-    let direction = "ascending";
-    if (sortConfig.key === key && sortConfig.direction === "ascending") {
-      direction = "descending";
-    }
-    setSortConfig({ key, direction });
-  };
-
-  const getSortIcon = (columnName) => {
-    if (sortConfig.key !== columnName) {
-      return <div className="w-4 h-4 inline-block" />;
-    }
-    return sortConfig.direction === "ascending" ? (
-      <ArrowUp className="w-4 h-4 inline-block" />
-    ) : (
-      <ArrowDown className="w-4 h-4 inline-block" />
-    );
-  };
+  const { sortedData, sortConfig, requestSort } = useSortData(data);
 
   return (
     <div className="text-center">
@@ -50,7 +12,7 @@ const PatientTable = ({ data }) => {
       </h2>
       <div className="overflow-x-auto">
         <table className="w-full">
-          <thead className="bg-cyan-50 border-[1px] border-opacity-15 border-[#000]">
+          <thead className="bg-cyan-50">
             <tr>
               <th className="px-6 py-3 text-sm font-semibold text-black border-[1px] border-opacity-15 border-[#000]">
                 Id
@@ -59,7 +21,7 @@ const PatientTable = ({ data }) => {
                 className="px-6 py-3 text-sm font-semibold text-black border-[1px] border-opacity-15 border-[#000] cursor-pointer"
                 onClick={() => requestSort("nama")}
               >
-                Nama {getSortIcon("nama")}
+                Nama {getSortIcon(sortConfig, "nama")}
               </th>
               <th className="px-6 py-3 text-sm font-semibold text-black border-[1px] border-opacity-15 border-[#000]">
                 Email
@@ -68,7 +30,7 @@ const PatientTable = ({ data }) => {
                 className="px-6 py-3 text-sm font-semibold text-black border-[1px] border-opacity-15 border-[#000] cursor-pointer"
                 onClick={() => requestSort("usia")}
               >
-                Usia {getSortIcon("usia")}
+                Usia {getSortIcon(sortConfig, "usia")}
               </th>
               <th className="px-6 py-3 text-sm font-semibold text-black border-[1px] border-opacity-15 border-[#000]">
                 No Telp
@@ -77,7 +39,7 @@ const PatientTable = ({ data }) => {
                 className="px-6 py-3 text-sm font-semibold text-black border-[1px] border-opacity-15 border-[#000] cursor-pointer"
                 onClick={() => requestSort("pekerjaan")}
               >
-                Pekerjaan {getSortIcon("pekerjaan")}
+                Pekerjaan {getSortIcon(sortConfig, "pekerjaan")}
               </th>
               <th className="px-6 py-3 text-sm font-semibold text-black border-[1px] border-opacity-15 border-[#000]">
                 Tanggal Lahir
@@ -90,34 +52,34 @@ const PatientTable = ({ data }) => {
               </th>
             </tr>
           </thead>
-          <tbody className="bg-cyan-50 divide-y divide-gray-200">
+          <tbody className="bg-cyan-50">
             {sortedData.map((patient) => (
               <tr key={patient.id}>
-                <td className="px-6 py-4 border-[1px] border-opacity-15 border-[#000]">
+                <td className="px-6 py-4 border-r border-l border-opacity-15 border-[#000]">
                   {patient.id}
                 </td>
-                <td className="px-6 py-4 border-[1px] border-opacity-15 border-[#000]">
+                <td className="px-6 py-4 border-r border-opacity-15 border-[#000]">
                   {patient.nama}
                 </td>
-                <td className="px-6 py-4 border-[1px] border-opacity-15 border-[#000]">
+                <td className="px-6 py-4 border-r border-opacity-15 border-[#000]">
                   {patient.email}
                 </td>
-                <td className="px-6 py-4 border-[1px] border-opacity-15 border-[#000]">
+                <td className="px-10 py-4 border-r border-opacity-15 border-[#000]">
                   {patient.usia}
                 </td>
-                <td className="px-6 py-4 border-[1px] border-opacity-15 border-[#000]">
+                <td className="px-6 py-4 border-r border-opacity-15 border-[#000]">
                   {patient.noTelp}
                 </td>
-                <td className="px-6 py-4 border-[1px] border-opacity-15 border-[#000]">
+                <td className="px-6 py-4 border-r border-opacity-15 border-[#000]">
                   {patient.pekerjaan}
                 </td>
-                <td className="px-6 py-4 border-[1px] border-opacity-15 border-[#000]">
+                <td className="px-6 py-4 border-r border-opacity-15 border-[#000]">
                   {patient.tanggalLahir}
                 </td>
-                <td className="px-6 py-4 border-[1px] border-opacity-15 border-[#000]">
+                <td className="px-6 py-4 border-r border-opacity-15 border-[#000]">
                   {patient.domisili}
                 </td>
-                <td className="px-6 py-4 border-[1px] border-opacity-15 border-[#000]">
+                <td className="px-6 py-4 border-r border-opacity-15 border-[#000]">
                   <div className="flex gap-3">
                     <button>
                       <img src="/images/admin/edit-button.svg" alt="edit" />
@@ -137,4 +99,3 @@ const PatientTable = ({ data }) => {
 };
 
 export default PatientTable;
-
