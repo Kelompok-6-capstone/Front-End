043312351fbc0ea9602c2021d1b48d975ca3cf9e
Arjun Nafi <arjunnafi04@gmail.com>
2024-12-04 09:54:25 +0700

    add admin protected routes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,45 +1,67 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from '../pages/auth/LoginPage';
-import RegisterPage from '../pages/auth/RegisterPage';
-import ForgotPasswordPage from '../pages/auth/ForgotPasswordPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "../pages/auth/LoginPage";
+import RegisterPage from "../pages/auth/RegisterPage";
+import ForgotPasswordPage from "../pages/auth/ForgotPasswordPage";
 import VerifyOtpPage from "../pages/auth/VerifyOTPPage";
-import LandingPage from '../pages/user/LandingPage';
-import Dashboard from '../pages/admin/Dashboard';
-import { UsersPage } from '../pages/admin/Users';
-import TransactionPage from '../pages/admin/Transactions';
-import DashboardUser from '../pages/user/DashboardUser';
-import ArtikelPage from '../pages/user/dashboard/ArtikelPage';
-import DetailArtikelPage from '../pages/user/dashboard/DetailArtikelPage';
-import DaftarPasien from '../pages/dokter/DaftarPasien';
+import LandingPage from "../pages/user/LandingPage";
+import Dashboard from "../pages/admin/Dashboard";
+import { UsersPage } from "../pages/admin/Users";
+import TransactionPage from "../pages/admin/Transactions";
+import DashboardUser from "../pages/user/DashboardUser";
+import ArtikelPage from "../pages/user/dashboard/ArtikelPage";
+import DetailArtikelPage from "../pages/user/dashboard/DetailArtikelPage";
+import DaftarPasien from "../pages/dokter/DaftarPasien";
 // import DetailPasien from '../pages/dokter/DetailPasien';
-import ProfileDokter from '../pages/dokter/ProfileDokter';
-
+import ProfileDokter from "../pages/dokter/ProfileDokter";
+import AdminLoginPage from "../pages/admin/AdminLogin";
+import AdminProtectedRoute from "./AdminProtectedRoute";
 
 const AppRoutes = () => (
-    <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-        <Route path="/verify-otp" element={<VerifyOtpPage />} />
+  <Routes>
+    <Route path="/" element={<LandingPage />} />
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/register" element={<RegisterPage />} />
+    <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+    <Route path="/verify-otp" element={<VerifyOtpPage />} />
 
-        {/* admin */}
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/admin/dashboard" element={<Dashboard />} />
-        <Route path="/admin/users" element={<UsersPage />} />
-        <Route path="/admin/transaction" element={<TransactionPage />} />
+    {/* admin */}
+    <Route path="/admin/login" element={<AdminLoginPage />} />
+    <Route
+      path="/admin/dashboard"
+      element={
+        <AdminProtectedRoute>
+          <Dashboard />
+        </AdminProtectedRoute>
+      }
+    />
+    <Route
+      path="/admin/users"
+      element={
+        <AdminProtectedRoute>
+          <UsersPage />
+        </AdminProtectedRoute>
+      }
+    />
+    <Route
+      path="/admin/transaction"
+      element={
+        <AdminProtectedRoute>
+          <TransactionPage />
+        </AdminProtectedRoute>
+      }
+    />
 
-        {/* user */}
-        <Route path="/user/dashboard" element={<DashboardUser />} />
-        <Route path="/user/artikel" element={<ArtikelPage />} />
-        <Route path="/artikel/:id" element={<DetailArtikelPage />} />
+    {/* user */}
+    <Route path="/user/dashboard" element={<DashboardUser />} />
+    <Route path="/user/artikel" element={<ArtikelPage />} />
+    <Route path="/artikel/:id" element={<DetailArtikelPage />} />
 
-        {/* dokter */}
-        <Route path='/dokter/daftar-passien' element={<DaftarPasien />} />
-        {/* <Route path='/dokter/detail-passien' element={<DetailPasien />} /> */}
-        <Route path='/dokter/profile-dokter' element={<ProfileDokter />} />
-    </Routes>
+    {/* dokter */}
+    <Route path="/dokter/daftar-passien" element={<DaftarPasien />} />
+    {/* <Route path='/dokter/detail-passien' element={<DetailPasien />} /> */}
+    <Route path="/dokter/profile-dokter" element={<ProfileDokter />} />
+  </Routes>
 );
 
 export default AppRoutes;
