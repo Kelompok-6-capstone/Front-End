180b9d5c8ae93e5349a7286e3a8b8d42aa559dc4
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-12-04 17:15:47 +0700

    Refactor Navbar component for improved structure and accessibility; enhance menu toggle functionality and update styling

--- src/components/user/Navbar.jsx
+++ src/components/user/Navbar.jsx
@@ -9,55 +9,47 @@ export default function Navbar() {
     };
 
     return (
-        <>
-            <header className="fixed top-0 left-0 w-full z-50 bg-white text-sm py-3 shadow-md">
-                <nav className="w-full mx-auto px-4 lg:px-24 sm:flex sm:items-center sm:justify-between">
-                    <div className="flex items-center justify-between">
-                        <a
-                            className="flex-none text-base font-semibold focus:outline-none focus:opacity-80"
-                            href="#"
-                            aria-label="Brand"
-                        >
-                            <img
-                                className="w-[100px] sm:w-[160px] h-auto"
-                                src="/images/logo.png"
-                                alt="Logo"
-                            />
-                        </a>
-                        <div className="sm:hidden">
-                            <button
-                                type="button"
-                                className="relative size-7 flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50"
-                                onClick={toggleMenu}
-                                aria-expanded={isMenuOpen}
-                                aria-controls="navbar-menu"
-                            >
-                                {isMenuOpen ? (
-                                    <img src="/images/user/cross_icon.svg" alt="" srcset="" />
-                                ) : (
-                                    <img src="/images/user/menu_icon.svg" alt="" srcset="" />
-                                )}
-                                <span className="sr-only">Toggle navigation</span>
-                            </button>
-                        </div>
-                    </div>
-                    <div
-                        id="navbar-menu"
-                        className={`${isMenuOpen ? 'block' : 'hidden'
-                            } overflow-hidden transition-all duration-300 basis-full grow sm:block`}
+        <header className="fixed top-0 left-0 w-full z-50 bg-white text-sm py-3 shadow-md">
+            <nav className="w-full mx-auto px-4 lg:px-24 sm:flex sm:items-center sm:justify-between">
+                <div className="flex items-center justify-between">
+                    <a
+                        className="flex-none text-base font-semibold"
+                        href="#"
+                        aria-label="Brand"
                     >
-                        <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
-                            <a href="#beranda" className="text-center text-cyan-950 text-base font-semibold">Beranda</a>
-                            <a href="#tentang" className="text-center text-cyan-950 text-base font-semibold">Tentang</a>
-                            <a href="#artikel" className="text-center text-cyan-950 text-base font-semibold">Artikel</a>
-                            <a href="#testimoni" className="text-center text-cyan-950 text-base font-semibold">Testimoni</a>
-                            <a href="#kontak" className="text-center text-cyan-950 text-base font-semibold">Kontak</a>
-                            {/* Tombol login untuk desktop */}
-                            <DropdownLogin />
-                        </div>
+                        <img
+                            className="w-[100px] sm:w-[160px] h-auto"
+                            src="/images/logo.png"
+                            alt="Logo"
+                        />
+                    </a>
+                    <div className="sm:hidden">
+                        <button
+                            type="button"
+                            className="flex items-center gap-x-2 rounded-lg border bg-white text-gray-800 shadow-sm hover:bg-gray-50"
+                            onClick={toggleMenu}
+                        >
+                            {isMenuOpen ? (
+                                <img src="/images/user/cross_icon.svg" alt="Close" />
+                            ) : (
+                                <img src="/images/user/menu_icon.svg" alt="Menu" />
+                            )}
+                        </button>
                     </div>
-                </nav>
-            </header>
-        </>
+                </div>
+                <div
+                    className={`${isMenuOpen ? 'block' : 'hidden'} transition-all sm:block`}
+                >
+                    <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 text-center md:text-left">
+                        <a href="#beranda" className="text-cyan-950 font-semibold text-center lg:text-left">Beranda</a>
+                        <a href="#tentang" className="text-cyan-950 font-semibold text-center lg:text-left">Tentang</a>
+                        <a href="#artikel" className="text-cyan-950 font-semibold text-center lg:text-left">Artikel</a>
+                        <a href="#testimoni" className="text-cyan-950 font-semibold text-center lg:text-left">Testimoni</a>
+                        <a href="#kontak" className="text-cyan-950 font-semibold text-center lg:text-left">Kontak</a>
+                        <DropdownLogin />
+                    </div>
+                </div>
+            </nav>
+        </header>
     );
 }
