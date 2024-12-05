cf7ef332978fcaa49362a891a182b8ea3ef4f5e8
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-12-05 16:57:32 +0700

    Refactor JanjiKonsultasiButton to use anchor tag and enhance BreadcrumbDaftarDokter styling with hover effects

--- src/components/user/beranda/JanjiKonsultasiButton.jsx
+++ src/components/user/beranda/JanjiKonsultasiButton.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export default function JanjiKosultasiButton() {
     return (
         <>
-            <Link
-                to="/konsultasi"
+            <a href='/user/dokter/detail-dokter'
                 className="w-28 h-[36.66px] px-[9.17px] py-[4.58px] bg-teal-900 rounded justify-center items-center gap-[14.89px] inline-flex hover:bg-teal-700 hover:scale-105 transition-transform duration-200 ease-in-out"
             >
                 <div className="text-center text-neutral-100 text-xs font-semibold">
                     Buat Janji
                 </div>
-            </Link>
+            </a>
         </>
     )
 }
--- src/components/user/dokter/BreadcrumbDaftarDokter.jsx
+++ src/components/user/dokter/BreadcrumbDaftarDokter.jsx
@@ -4,15 +4,20 @@ import { Link } from 'react-router-dom'
 export default function BreadcrumbDaftarDokter() {
     return (
         <>
-            <ol className="flex items-center whitespace-nowrap mb-5 ">
+            <ol className="flex items-center whitespace-nowrap mb-5">
                 <li className="inline-flex items-center">
                     <Link
-                        className="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:text-neutral-500 dark:hover:text-blue-500 dark:focus:text-blue-500"
+                        className="flex items-center text-sm text-cyan-900 relative group focus:outline-none"
                         to="/user/beranda"
                     >
                         Beranda
+                        <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-gradient-to-r from-cyan-400 to-blue-600 transition-all duration-300 group-hover:w-full"></span>
                     </Link>
-                    <img src="/images/user/artikel/slash.svg" alt="gambar slash" />
+                    <img
+                        className="ml-2 transform transition-transform duration-300 group-hover:scale-125"
+                        src="/images/user/artikel/slash.svg"
+                        alt="gambar slash"
+                    />
                 </li>
                 <li
                     className="inline-flex items-center text-sm font-semibold text-gray-800 truncate dark:text-neutral-200"
