841843d8c2b01d3d7a20c4590dc243882cb3d3e9
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-12-05 10:06:49 +0700

    Add doctor listing and detail components with breadcrumb navigation

---
+++ src/pages/user/DetailDokterPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DetailDokter from '../../components/user/dokter/DetailDokter'
+
+export default function DetailDokterPage() {
+    return (
+        <>
+            <DetailDokter />
+        </>
+    )
+}
