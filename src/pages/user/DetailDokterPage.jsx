77b24c74056cc5c29d43a5746e52de1860c7fe6b
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-12-05 16:57:47 +0700

    Add DetailDokterPage with Breadcrumb, Navbar, and Footer; enhance DetailDokter layout and introduce CatatanSebelumKonsultasi component

--- src/pages/user/DetailDokterPage.jsx
+++ src/pages/user/DetailDokterPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import DetailDokter from '../../components/user/dokter/DetailDokter'
+import Navbar from '../../components/user/beranda/Navbar'
+import Footer from '../../components/user/Footer'
+import BreadcrumbDetailDokter from '../../components/user/dokter/BreadcrumbDetailDokter'
+import CatatanSebelumKonsultasi from '../../components/user/dokter/CatatanSebelumKonsultasi'
 
 export default function DetailDokterPage() {
     return (
         <>
-            <DetailDokter />
+            <Navbar />
+            <div className="lg:px-[100px] p-3 lg:me-5 mt-20 lg:mt-24">
+                <BreadcrumbDetailDokter />
+                <DetailDokter />
+                <CatatanSebelumKonsultasi />
+            </div>
+            <Footer />
         </>
     )
 }
