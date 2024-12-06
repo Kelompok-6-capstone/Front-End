b23ead638263955db6dbcc11d3a34bf8feda240c
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-12-06 07:58:58 +0700

    Refactor DaftarSemuaArtikel component to use useFetchArticles hook for data fetching and update article properties for rendering

--- src/components/user/artikel/DaftarSemuaArtikel.jsx
+++ src/components/user/artikel/DaftarSemuaArtikel.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import ArtikelCard from './ArtikelCard';
-import articles from '../../../data/user/artikelData';
+import useFetchArticles from '../../../hooks/useFetchArticles';
 
 const DaftarSemuaArtikel = () => {
+    const { articles, loading, error } = useFetchArticles('https://api.calmind.site/user/artikel');
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+
     return (
         <section id="artikel" className="bg-white p-5 md:p-10">
             <div className="text-black text-2xl font-medium mb-4">Artikel Terbaru</div>
-            <div
-                className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6"
-                aria-label="Daftar artikel"
-            >
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6" aria-label="Daftar artikel">
                 {articles.map((article) => (
                     <ArtikelCard
                         key={article.id}
                         id={article.id}
-                        title={article.title}
-                        date={article.date}
-                        description={article.description}
-                        image={article.image}
+                        title={article.judul}
+                        date={article.created_at}
+                        description={article.isi}
+                        image={article.gambar}
                     />
                 ))}
             </div>
