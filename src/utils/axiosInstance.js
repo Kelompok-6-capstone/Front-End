28fcdd0f77942083c3b936d1fa3c5012e3bfd12b
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-12-06 14:07:27 +0700

    Add Authorization header to axios instance for token-based authentication

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,8 +4,11 @@ const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_BASE_URL,
     headers: {
         "Content-Type": "application/json",
+        // Tambahkan Authorization jika diperlukan
+        Authorization: `Bearer ${localStorage.getItem("token_user") || ""}`,
     },
     withCredentials: true,
 });
+
 
 export default axiosInstance;
