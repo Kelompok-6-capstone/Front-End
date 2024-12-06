9b7f639480b78e618707e16bd84b6c1f96bb0988
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-12-06 07:58:42 +0700

    Refactor ArtikelBeranda component to use useFetchArticles hook for data fetching

--- src/components/user/artikel/ArtikelBeranda.jsx
+++ src/components/user/artikel/ArtikelBeranda.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import ArtikelList from './ArtikelList';
-import articles from '../../../data/user/artikelData';
+import useFetchArticles from '../../../hooks/useFetchArticles';
 
 const ArtikelBeranda = () => {
+    const { articles, loading, error } = useFetchArticles('https://api.calmind.site/user/artikel');
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
 
     return (
         <section id="artikel" className="bg-white p-5 md:p-0 mt-12">
-            <div className="">
+            <div>
                 <div className="flex justify-between items-center mb-6">
                     <div className="text-cyan-900 text-base font-semibold">Artikel Terkini</div>
-                    <a href='/user/artikel'>
+                    <a href="/user/artikel">
                         <div className="text-cyan-900 text-base font-semibold hover:underline">
                             Lihat Semua
                         </div>
