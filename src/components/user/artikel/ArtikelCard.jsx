96369c72a6b8109d7b3b06dc735342564237121e
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-12-08 16:02:04 +0700

    fix: update link path for artikel detail in ArtikelCard component

--- src/components/user/artikel/ArtikelCard.jsx
+++ src/components/user/artikel/ArtikelCard.jsx
@@ -17,7 +17,7 @@ const ArtikelCard = ({ id, title, date, description, image }) => {
                 <h3 className="text-cyan-950 text-lg font-semibold mb-2">{title}</h3>
                 <p className="text-neutral-500 text-sm mb-4">{date}</p>
                 <p className="text-cyan-950 text-sm mb-4">{truncatedDescription}</p>
-                <Link to={`/artikel/${id}`}>
+                <Link to={`/user/artikel/${id}`}>
                     <button className="text-blue-500 font-bold text-sm hover:underline">
                         Lihat Detail →
                     </button>
