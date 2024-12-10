8934103f9d9b8ede18076694aa5404a1fccc5e4a
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-12-10 17:02:53 +0700

    feat: update JanjiKonsultasiButton to utilize doctor data for dynamic link generation

--- src/components/user/beranda/JanjiKonsultasiButton.jsx
+++ src/components/user/beranda/JanjiKonsultasiButton.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from "react";
+export default function JanjiKonsultasiButton({ dokter }) {
+    if (!dokter) return null;
 
-export default function JanjiKosultasiButton() {
     return (
-        <>
-            <a href='/user/dokter/detail-dokter'
-                className="w-28 h-[36.66px] px-[9.17px] py-[4.58px] bg-teal-900 rounded justify-center items-center gap-[14.89px] inline-flex hover:bg-teal-700 hover:scale-105 transition-transform duration-200 ease-in-out"
-            >
-                <div className="text-center text-neutral-100 text-xs font-semibold">
-                    Buat Janji
-                </div>
-            </a>
-        </>
-    )
+        <a
+            href={`/user/dokter/${dokter.id}`}
+            className="w-28 h-[36.66px] px-[9.17px] py-[4.58px] bg-teal-900 rounded justify-center items-center gap-[14.89px] inline-flex hover:bg-teal-700 hover:scale-105 transition-transform duration-200 ease-in-out"
+        >
+            <div className="text-center text-neutral-100 text-xs font-semibold">Buat Janji</div>
+        </a>
+    );
 }
