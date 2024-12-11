4bf2eecaae36f8d2be3b1be82299929dc5d5a9ec
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-12-11 11:20:20 +0700

    feat: add ChatbotUser component to BerandaUserPage

--- src/pages/user/BerandaUserPage.jsx
+++ src/pages/user/BerandaUserPage.jsx
@@ -5,10 +5,12 @@ import TagSection from '../../components/user/beranda/TagSection'
 import JanjiKosultasi from '../../components/user/beranda/JanjiKonsultasi'
 import ArtikelBeranda from '../../components/user/artikel/ArtikelBeranda'
 import Footer from '../../components/user/Footer'
+import ChatbotUser from '../../components/user/beranda/ChatbotUser'
 
 export default function BerandaUserPage() {
     return (
         <>
+            <ChatbotUser />
             <Navbar />
             <div className="lg:px-[100px] lg:me-5 ">
                 <HeroSection />
