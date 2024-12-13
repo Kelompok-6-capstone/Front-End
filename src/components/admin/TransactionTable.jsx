aca91f969cb2f65598ccdd6b02d2ac885acc94b2
Arjun Nafi <arjunnafi04@gmail.com>
2024-12-13 21:48:00 +0700

    styiling approve button

--- src/components/admin/TransactionTable.jsx
+++ src/components/admin/TransactionTable.jsx
@@ -207,14 +207,14 @@ const TransactionTable = () => {
                     {transaction.status === "pending" ? (
                       <button
                         onClick={() => handleStatusChange(transaction.id)}
-                        className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-medium py-1 px-2 rounded"
+                        className="bg-[#14B8A6] hover:bg-[#1e635b] text-white text-sm font-medium py-2 px-3 rounded-lg"
                       >
                         Approve
                       </button>
                     ) : (
                       <button
                         disabled
-                        className="bg-gray-300 text-gray-500 text-sm font-medium py-1 px-2 rounded cursor-not-allowed"
+                        className="bg-gray-300 text-gray-500 text-sm font-medium py-2 px-3 rounded-lg cursor-not-allowed"
                       >
                         Approved
                       </button>
