57749ca05c0d0804ccad09464d201584166af659
Prasetyo Edi Pamungkas <prasetyoedi451@gmail.com>
2024-12-15 21:11:02 +0700

    refactor: remove console logs from OTP verification API

--- src/api/auth/verifyOtp.js
+++ src/api/auth/verifyOtp.js
@@ -6,10 +6,10 @@ import axiosInstance from "../../utils/axiosInstance";
  * @returns {Promise} - Response dari server.
  */
 export const verifyOtp = async ({ email, code }) => {
-    console.log("Mengirim data ke API:", { email, code });
+    // console.log("Mengirim data ke API:", { email, code });
     try {
         const response = await axiosInstance.post("/user/verify-otp", { email, code });
-        console.log("Respons dari server:", response.data);
+        // console.log("Respons dari server:", response.data);
         return response.data;
     } catch (error) {
         console.error("Error dari API:", error.response || error.message);
